e907440a01e6ec6a14c0ce96ba592bd65e39a357
chen521xin <1334870080@qq.com>
2017-11-24 17:43:45 +0800

    添加config

--- boss-config/src/main/java/com/boss/config/ConfigApplication.java
+++ boss-config/src/main/java/com/boss/config/ConfigApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * ${DESCRIPTION}
@@ -12,7 +12,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * @author wanghaobin
  * @create 2017-05-25 12:44
  */
-@EnableEurekaServer // 启动一个服务注册中心提供给其他应用进行对话
+@EnableEurekaClient // 启动一个服务注册中心提供给其他应用进行对话
 @EnableDiscoveryClient
 @EnableConfigServer
 @SpringBootApplication
--- ufms-client/src/main/java/com/boss/config/CenterBootstrap.java
+++ ufms-client/src/main/java/com/boss/config/CenterBootstrap.java
@@ -3,7 +3,6 @@ package com.boss.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * ${DESCRIPTION}
@@ -13,7 +12,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @EnableEurekaServer  //启动一个服务注册中心提供给其他应用进行对话
 @SpringBootApplication
-@EnableZuulProxy
 public class CenterBootstrap {
     public static void main(String[] args) {
         SpringApplication.run(CenterBootstrap.class, args);
